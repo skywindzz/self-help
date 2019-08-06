c85b81af7c42e4e005d89dc4e17a369f7379e5ac
skywindzz <skywindzz@yahoo.com>
2019-08-05 18:44:01 -0700

    8/5

--- src/components/newhabitcard.js
+++ src/components/newhabitcard.js
@@ -1,54 +1,45 @@
 import React, { useState } from 'react'
-
-
-class Example extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        count: 0
-      };
-    }
   
 class NewHabitCard extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            habitName = '',
-            priority = 0,
-            inverse = false,
-            fourLaws =[],
-            duration = '',
-            time = ''
+            habitName : '',
+            priority : 0,
+            inverse : false,
+            fourLaws :[],
+            duration : '',
+            time : ''
         };
+
+        function submitHabit () {
+            console.log('setting up here');
+        } 
     }
     
-    
-    {/*in this component you'll take the input of the user and save it into the habitCard object 
-on the parent which is the */}
-    
-    function submitHabit () {
-        console.log('setting up here')
-    } 
+
+    render() {
     return (
         <div css={{ lineHeight: '2.3rem', fontFamily: 'sans-serif', display: 'grid', borderStyle: 'solid', borderWidth: '1px', borderRadius: '10px', width: '80%', justifyContent: 'center', boxShadow: '5px 10px #aaaaaa' }}>
             <span css={{ fontSize: '2.5em', marginTop: '0.5em', marginBottom: '0.5em' }}>New Habit Card</span>
-            <div>Name: <input value={habitName} /></div>
-            <div>Priority(1 - 5): <input value={priority} /> </div>
+            <div>Name: <input value={this.habitName} /></div>
+            <div>Priority(1 - 5): <input value={this.priority} /> </div>
             
-            <div>Inverse habit?: <input type='checkbox' value={inverse}/></div>
+            <div>Inverse habit?: <input type='checkbox' value={this.inverse} /></div>
             <div>
-                <div>Make it obvious: <input value={fourLaws[0]} /></div>
-                <div>Make it attractive: <input value={fourLaws[1]} /></div>
-                <div>Make it easy: <input value={fourLaws[2]} /></div>
-                <div>Make it satisfying:<input value={fourLaws[3]} /></div>
+                <div>Make it obvious: <input value={this.fourLaws[0]} /></div>
+                <div>Make it attractive: <input value={this.fourLaws[1]} /></div>
+                <div>Make it easy: <input value={this.fourLaws[2]} /></div>
+                <div>Make it satisfying:<input value={this.fourLaws[3]} /></div>
             </div>
 
-            <div>Minimum Duration: <input value={duration} /></div>
-            <div>Scheduled Time : <input value={time} /> </div>
+            <div>Minimum Duration: <input value={this.UNSAFE_componentWillMountduration} /></div>
+            <div>Scheduled Time : <input value={this.time} /> </div>
 
-            <button onClick={submitHabit} css={{ marginBottom: '2rem', borderColor: 'lightgreen' }}>submit habit card</button>
+            <button onClick={this.submitHabit} css={{ marginBottom: '2rem', borderColor: 'lightgreen' }}>submit habit card</button>
         </div>
     )
+    }
 }
 
-export default HabitCard
+export default NewHabitCard
